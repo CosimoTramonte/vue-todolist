c7026f13ec1ed50f545b87f2d2aa5cbc89aca290
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-14 16:15:19 +0200

    finish work

--- js/script.js
+++ js/script.js
@@ -5,24 +5,68 @@ createApp({
   data(){
 
     return {
-      tasks:[
+
+        tasks:[
         {
-            text: "Task1",
+            text: "Fare gli esercizi",
             complete: false
         },
         {
-            text: "Task2",
+            text: "Andare a fare la spesa",
             complete: false
         },
         {
-            text: "Task3",
+            text: "Cena con gli amici",
             complete: false
         },
         {
-            text: "Task4",
+            text: "Comprare un vestito",
             complete: false
         },
-      ]
+    ],
+    message: "",
+    newTaskText: ""
+    }
+  }, 
+
+  methods:{
+
+    remouveTask(index){
+
+        if(!this.tasks[index].complete){
+
+            this.message = `Mi dispiace, devi completare prima la task per poterla eliminare`
+
+            setTimeout(() => {
+                this.message= ""
+            }, 3000)
+
+        } else {
+
+            this.tasks.splice(index,1)
+            this.message= "";
+        }
+    },
+
+    addTask(){
+
+        if(this.newTaskText.length < 4){
+            this.message = `Mi dispiace, devi inserire un testo di almeno 5 caratteri`
+
+            setTimeout(() => {
+                this.message= ""
+            }, 3000)
+
+        } else {
+
+            const newTask = {
+                text: this.newTaskText,
+                complete: false,
+            }
+
+            this.tasks.unshift(newTask)
+            this.newTaskText= "";
+        }
     }
   }
 
